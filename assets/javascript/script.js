1b5219823961051f37136e34db09d7873cab628f
wylie <wyliedavid1984@gmail.com>
2020-12-01 15:51:04 -0500

    Intial commit with file structure and question array completed

---
+++ assets/javascript/script.js
@@ -0,0 +1,56 @@
+// variables
+
+var questionBank = [
+    {
+        number: 1,
+        question: "Commonly used data types do not include ____ ?",
+        answer: "Alerts",
+        falseAnswers: ["Booleans", "Strings", "Integers"]
+    }, {
+        number: 2,
+        question: "The condition in an if statement is inclose by ____ ?",
+        answer: "Parentheses",
+        falseAnswers: ["Curly Brackets", "Brackets", "Quotations"]
+    }, {
+        number: 3,
+        question: "Arrays in javascript are used to store _____ ?",
+        answer: "All of the above",
+        falseAnswers: ["Booleans", "Strings", "Integers"]
+    }, {
+        number: 4,
+        question: "Strings are enclosed by ____, when being stored in a variable.",
+        answer: "Quotations",
+        falseAnswers: ["Brackets", "Curly Brackets", "Parentheses"]
+    }, {
+        number: 5,
+        question: "Where is the code of a function enclosed by ____ ?",
+        answer: "Curly Brackets",
+        falseAnswers: ["Brackets", "Quotations", "Parentheses"]
+    }, {
+        number: 6,
+        question: "Where do we store data or values?",
+        answer: "Variable",
+        falseAnswers: ["For Loops", "If Statements", "Functions"]
+    }, {
+        number: 7,
+        question: "What is the counter in a for loop called",
+        answer: "Iterator",
+        falseAnswers: ["Variable", "Function", "For Loops"]
+    }, {
+        number: 8,
+        question: "Which isn't one of the three tools used to create websites?",
+        answer: "Linux",
+        falseAnswers: ["HTML", "CSS", "Javascript"]
+    }, {
+        number: 9,
+        question: "Where would we store a list or several values in a variable",
+        answer: "Array",
+        falseAnswers: ["Functions", "For Loops", "HTML"]
+    }, {
+        number: 10,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "Console logging",
+        falseAnswers: ["For Loops", "Javascript", "Conditionals"]
+    },
+];
+var timer = 240;

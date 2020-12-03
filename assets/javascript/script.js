3918bc7c3941fb1ca96eefebe842be15110c2773
wylie <wyliedavid1984@gmail.com>
2020-12-03 15:40:32 -0500

    added in some comments in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -119,25 +119,26 @@ var i = 0;
 var countdown = 75;
 
 // appending all our new elements
-
-answerButtons.appendChild(answers);
-// for( var j = 0; j<questionBank[i].answers.length; j++){
-//      answers.appendChild(answer[i])
-// }
-answers.appendChild(answer1);
-answers.appendChild(answer2);
-answers.appendChild(answer3);
-answers.appendChild(answer4);
-
-// setting some style to hide buttons
-// for(var j=0; j<questionBank[i].answers.length; j++){
-//  answer[j].setAttribute("style", "display:none")
-// }
-answer1.setAttribute("style", "display:none");
-answer2.setAttribute("style", "display:none");
-answer3.setAttribute("style", "display:none");
-answer4.setAttribute("style", "display:none");
-
+if (answerButtons) {
+    answerButtons.appendChild(answers);
+    // for( var j = 0; j<questionBank[i].answers.length; j++){
+    //      answers.appendChild(answer[i])
+    // }
+    answers.appendChild(answer1);
+    answers.appendChild(answer2);
+    answers.appendChild(answer3);
+    answers.appendChild(answer4);
+}
+if (answers) {
+    // setting some style to hide buttons
+    // for(var j=0; j<questionBank[i].answers.length; j++){
+    //  answer[j].setAttribute("style", "display:none")
+    // }
+    answer1.setAttribute("style", "display:none");
+    answer2.setAttribute("style", "display:none");
+    answer3.setAttribute("style", "display:none");
+    answer4.setAttribute("style", "display:none");
+}
 
 // Creates a timer for the user to complete the questions
 function timerCountdown() {
@@ -181,16 +182,19 @@ function startGame() {
 
 // This function generates a question from the question bank. 
 function generateQuestion() {
+
+    questionNumber.textContent = "Question   " + questionBank[i].number;
+    theQuestion.textContent = questionBank[i].question;
+    // dry code if i can get the rest of my application working.
     // if i can get this reference to each answer then the for loop works.
     // for(var j =0; j<questionBanki[i].answers.length; j++){
-
     // answer[j].textcontent = questionBank[i].answers[j];
     // answer[j].setAttribute("value", questionBank[i].answer[j]);
     // answer[j].addEventListener("click", keepScore);
     // }
-    questionNumber.textContent = "Question   " + questionBank[i].number;
-    theQuestion.textContent = questionBank[i].question;
-
+    // The following sets the content to the array of answers
+    // then sets the value of the button.
+    // Sets the event listener for the click.
     answer1.textContent = questionBank[i].answers[0];
     answer1.setAttribute("value", questionBank[i].answers[0]);
     answer1.addEventListener("click", keepScore);
@@ -215,19 +219,22 @@ function generateQuestion() {
 // keeps score of the user by return the score after it has been altered, and will also alert user if the answer what correct or wrong. I then generates another question by calling generateQuestion
 
 function keepScore() {
-
+    // sets answer to correct answer.
     var answer = questionBank[i].correctAnswer;
 
+    // checking if correct answer if selected and incrementing score
+    // else decrement the count down. 
     if (this.value == answer) {
         score += 10;
     } else {
         countdown -= 10;
     }
+    // once question run out run game over function
     console.log(score)
     if (i == questionBank.length - 1) {
         return gameOver();
     }
-    
+    // increment i and run generateQuestion function
     i++;
     generateQuestion();
 
@@ -235,6 +242,11 @@ function keepScore() {
 
 // It creates the game over screen with a way to log in the users score to high scores.
 function gameOver() {
+
+    // setting some style to hide buttons
+    // for(var j=0; j<questionBank[i].answers.length; j++){
+    //  answer[j].setAttribute("style", "display:none")
+    // }
     answer1.setAttribute("style", "display:none");
     answer2.setAttribute("style", "display:none");
     answer3.setAttribute("style", "display:none");
@@ -246,31 +258,49 @@ function gameOver() {
 
 document.getElementById("quizOverScreen").addEventListener("submit", function (event) {
     event.preventDefault();
-    var userName = document.getElementById("userName").value;
-    var highScore = document.getElementById("highScores");
-    var userData = document.getElementById("userData");
-    
-    var userList = document.createElement("ol");
-    // create a for loop to create lis for data
-    var user1 = document.createElement("li");
-    // check if data exists in local storage get data and set data to object. parse here and again
+    var userName = document.getElementById("userName").value.trim();
+    var userData = {
+        name: userName,
+        score: score
+    };
+    var userList = [];
+
     var userDataList = JSON.parse(localStorage.getItem("userData"));
+
+    if (userDataList) {
+        userData = userDataList;
+        userList.push(userData);
+    }
+
+    // if value doesn't exist in local storage then need to stringify and set to local storage.
+    localStorage.setItem("userData", JSON.stringify(userData), JSON.stringify(userDataList))
+    postScores();
+})
+
+function postScores() {
+
+    var highScore = document.getElementById("highScores");
+    var userList = document.createElement("ol");
+
+
+    // loop through local storage to create each line.
+    for (var i = 0; i < )
+
+        var user1 = document.createElement("li");
+    // check if data exists in local storage get data and set data to object. parse here and again
+
+
+
+
     // appending created elements
     highScore.appendChild(userList);
     // create a for loop to append data
     userList.appendChild(user1);
 
-    var userData = {
-        name: userName,
-        score: score
-    };
-    // if value doesn't exist in local storage then need to stringify and set to local storage.
-    localStorage.setItem("userData", JSON.stringify(userData));
 
-
-})
-
-function postScores() {
     // check if there is data in local storage. if data is there then parse the data and build array.
     // loop through an array to set values to 
+    var userDataList = JSON.parse(localStorage.getItem("userData"));
+
+
 }

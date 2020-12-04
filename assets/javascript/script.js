2254371503665eb373c16d9a7a87329077158145
wylie <wyliedavid1984@gmail.com>
2020-12-04 10:10:01 -0500

    added funcitonality to clear button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -101,14 +101,16 @@ var questionBank = [{
     ],
     correctAnswer: "Console logging"
 }];
-
+var i = 0;
 var timerEl = document.getElementById("timer");
 var questionNumber = document.getElementById("questionNum");
 var theQuestion = document.getElementById("theQuestion");
 var answers = document.createElement("ul");
 // for (var j = 0; j < questionBank[i].answerBank.length; j++) {
-//     var answer = answer[j];
-//     answer[j] = document.createElement("button");
+//     var answer = questionBank[i].answerBank[j];
+//     var answerButton = document.createElement("button");
+//     answerButton.innerHTML = answer;
+//     answers.appendChild(answer);
 // }
 var answer1 = document.createElement("button");
 var answer2 = document.createElement("button");
@@ -116,11 +118,9 @@ var answer3 = document.createElement("button");
 var answer4 = document.createElement("button");
 var answerButtons = document.getElementById("answerBtns");
 var score = 0;
-var i = 0;
 var countdown = 75;
 var userDataList = JSON.parse(localStorage.getItem("userData")) || [];
 var userArray = [];
-
 
 // appending all our new elements
 if (answerButtons) {
@@ -179,7 +179,7 @@ function startGame() {
     answer2.removeAttribute("style", "display:none");
     answer3.removeAttribute("style", "display:none");
     answer4.removeAttribute("style", "display:none");
-   
+
     timerCountdown();
     generateQuestion();
 }
@@ -202,110 +202,119 @@ function generateQuestion() {
     // The following sets the content to the array of answers
     // then sets the value of the button.
     // Sets the event listener for the click.
-        answer1.textContent = questionBank[i].answerBank[0];
-        answer1.setAttribute("value", questionBank[i].answerBank[0]);
-        answer1.addEventListener("click", keepScore);
+    answer1.textContent = questionBank[i].answerBank[0];
+    answer1.setAttribute("value", questionBank[i].answerBank[0]);
+    answer1.addEventListener("click", keepScore);
 
-        answer2.textContent = questionBank[i].answerBank[1];
-        answer2.setAttribute("value", questionBank[i].answerBank[1]);
-        answer2.addEventListener("click", keepScore);
+    answer2.textContent = questionBank[i].answerBank[1];
+    answer2.setAttribute("value", questionBank[i].answerBank[1]);
+    answer2.addEventListener("click", keepScore);
 
-        answer3.textContent = questionBank[i].answerBank[2];
-        answer3.setAttribute("value", questionBank[i].answerBank[2]);
-        answer3.addEventListener("click", keepScore);
+    answer3.textContent = questionBank[i].answerBank[2];
+    answer3.setAttribute("value", questionBank[i].answerBank[2]);
+    answer3.addEventListener("click", keepScore);
 
-        answer4.textContent = questionBank[i].answerBank[3];
-        answer4.setAttribute("value", questionBank[i].answerBank[3])
-        answer4.addEventListener("click", keepScore);
+    answer4.textContent = questionBank[i].answerBank[3];
+    answer4.setAttribute("value", questionBank[i].answerBank[3])
+    answer4.addEventListener("click", keepScore);
 
+}
+
+// keeps score of the user by return the score after it has been altered, and will also alert user if the answer what correct or wrong. I then generates another question by calling generateQuestion
+
+function keepScore() {
+    // sets answer to correct answer.
+
+    var answer = questionBank[i].correctAnswer;
+
+    // checking if correct answer if selected and incrementing score
+    // else decrement the count down. 
+    if (this.value == answer) {
+        score += 10;
+    } else {
+        countdown -= 10;
     }
-
-    // keeps score of the user by return the score after it has been altered, and will also alert user if the answer what correct or wrong. I then generates another question by calling generateQuestion
-
-    function keepScore() {
-        // sets answer to correct answer.
-        
-        var answer = questionBank[i].correctAnswer;
-
-        // checking if correct answer if selected and incrementing score
-        // else decrement the count down. 
-        if (this.value == answer) {
-            score += 10;
-        } else {
-            countdown -= 10;
-        }
-        // once question run out run game over function
-        console.log(score)
-        if (i == questionBank.length - 1) {
-            countdown = 0;
-            return gameOver();
-        }
-        // increment i and run generateQuestion function
-        i++;
-        generateQuestion();
-
+    // once question run out run game over function
+    console.log(score)
+    if (i == questionBank.length - 1) {
+        countdown = 0;
+        return gameOver();
     }
+    // increment i and run generateQuestion function
 
-    // It creates the game over screen with a way to log in the users score to high scores.
-    function gameOver() {
+    i++;
+    generateQuestion();
+}
 
-        // setting some style to hide buttons
-        // for (var j = 0; j < questionBank[i].answerBank.length; j++) {
-        //     answer = answer[j]
-        //     answer.setAttribute("style", "display:none");
-        // };
-        answer1.setAttribute("style", "display:none");
-        answer2.setAttribute("style", "display:none");
-        answer3.setAttribute("style", "display:none");
-        answer4.setAttribute("style", "display:none");
+// It creates the game over screen with a way to log in the users score to high scores.
+function gameOver() {
 
-        document.getElementById('quizScreen').classList.add('hidden');
-        document.getElementById('quizOverScreen').classList.remove('hidden');
+    // setting some style to hide buttons
+    // for (var j = 0; j < questionBank[i].answerBank.length; j++) {
+    //     answer = answer[j]
+    //     answer.setAttribute("style", "display:none");
+    // };
+    answer1.setAttribute("style", "display:none");
+    answer2.setAttribute("style", "display:none");
+    answer3.setAttribute("style", "display:none");
+    answer4.setAttribute("style", "display:none");
+    document.getElementById('quizScreen').classList.add('hidden');
+    document.getElementById('quizOverScreen').classList.remove('hidden');
 
-    }
+}
 
-    document.getElementById("quizOverScreen").addEventListener("submit", function (event) {
-        event.preventDefault();
-        document.getElementById("quizOverScreen").classList.add("hidden");
-        document.getElementById('ScoreScreen').classList.remove("hidden");
-        // getting the value of input, add to object, the push to user array.
-        var userName = document.getElementById("userName").value.trim();
-        var userData = [{
-            name: userName,
-            score: score
-        }]
+document.getElementById("quizOverScreen").addEventListener("submit", function (event) {
+    event.preventDefault();
+    document.getElementById("quizOverScreen").classList.add("hidden");
+    document.getElementById('ScoreScreen').classList.remove("hidden");
+    // getting the value of input, add to object, the push to user array.
+    var userName = document.getElementById("userName").value.trim();
+    var userData = [{
+        name: userName,
+        score: score
+    }]
+    userArray.push(...userData);
+    // to check if anything is local storage and then push up to user array.
+    var userDataList = JSON.parse(localStorage.getItem("userData"));
+
+    if (userDataList) {
+        userData = userDataList;
         userArray.push(...userData);
-        // to check if anything is local storage and then push up to user array.
-        var userDataList = JSON.parse(localStorage.getItem("userData"));
-      
-        if (userDataList) {
-            userData = userDataList;
-            userArray.push(...userData);
-        }
-        
-        // Stringify array and setting to local storage.
-        localStorage.setItem("userData", JSON.stringify(userArray))
-        postScores();
-    })
+    }
+    // Stringify array and setting to local storage.
+    localStorage.setItem("userData", JSON.stringify(userArray))
+    postScores();
+})
 
-    function postScores() {
-        // creating variables to the dom.
-        var highScore = document.getElementById("Scores");
-        var userList = document.createElement("ol");
+function postScores() {
+    // creating variables to the dom.
+    var highScore = document.getElementById("Scores");
+    var userList = document.createElement("ol");
 
-        // appending elements.
-        highScore.appendChild(userList);
-        
-        // loop through local storage to create each li and then adding content and lastly appending to dom.
-        for (var i = 0; i < userArray.length; i++) {
+    // appending elements.
+    highScore.appendChild(userList);
 
-            var user = document.createElement("li");
+    // loop through local storage to create each li and then adding content and lastly appending to dom.
+    for (var i = 0; i < userArray.length; i++) {
 
-            user.textContent = "User Name: " + userArray[i].name + " " + "Score: " + userArray[i].score;
+        var user = document.createElement("li");
 
-            userList.appendChild(user);
-        }
+        user.textContent = "User Name: " + userArray[i].name + " " + "Score: " + userArray[i].score;
 
+        userList.appendChild(user);
     }
 
+}
 
+document.getElementById("clearHighScores").addEventListener("click", function(){
+    
+    localStorage.clear();
+    
+    var messageDiv = document.getElementById("clearHighScoreMessage");
+    var messageClear = document.createElement("p");
+    messageClear.textContent = "The Scores will be clear out next round good Luck!!"
+
+    messageDiv.appendChild(messageClear);
+    
+
+})
